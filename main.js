347e5c37db45a32508e184912107702aebcec58f
Rpg87 <sra.picadillo@gmail.com>
2023-05-11 15:53:34 +0200

    add a hammer pointer

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const cursor = document.querySelector('.cursor')
+const holes = [...document.querySelectorAll('.hole')]
+
+
+window.addEventListener('mousemove', e => {
+    cursor.style.top = e.pageY + 'px' //up, down
+    cursor.style.left = e.pageX + 'px' //left, right
+})
+window.addEventListener('mousedown', () => {
+    cursor.classList.add('active')
+})
+window.addEventListener('mouseup', () => {
+    cursor.classList.remove('active')
+})
